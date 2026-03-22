a5158e5fa59420d7121a6397ba9d5cdd41d282c8
m187354123-commits <m187354123@gmail.com>
2026-03-22 14:47:10 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const ip =
+    req.headers['x-forwarded-for'] ||
+    req.socket.remoteAddress;
+
+  console.log("접속한 IP:", ip);
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <h1>Welcome 😎</h1>
+    <p>접속 기록 중...</p>
+  `);
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("서버 실행 중");
+});
