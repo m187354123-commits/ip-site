8c928d3eb3bca72d0b13834fc14f54dfcb43601d
m187354123-commits <m187354123@gmail.com>
2026-03-22 15:09:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   const ip =
-    req.headers['x-forwarded-for'] ||
-    req.socket.remoteAddress;
+    req.headers['x-forwarded-for']
+      ? req.headers['x-forwarded-for'].split(',')[0].trim()
+      : req.socket.remoteAddress;
 
   console.log("접속한 IP:", ip);
 
@@ -11,6 +12,7 @@ const server = http.createServer((req, res) => {
   res.end(`
     <h1>Welcome 😎</h1>
     <p>접속 기록 중...</p>
+    <p>IP: ${ip}</p>
   `);
 });
 
